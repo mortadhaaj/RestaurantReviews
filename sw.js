3c8ae0cea40e218810c0e03452fe1372d1c06fbf
Murtadha Aljubran <31464529+mortadhaaj@users.noreply.github.com>
2019-06-23 17:32:15 +0300

    Uploading Restaurants Reviews Files

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event) {
+  event.waintUntil(
+    caches.open('mycache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'css/sytles.css',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'sw.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+ event.respondWith(
+ 	caches.open('mycache').then(function(cache) {
+		return caches.match(event.request).then(function(response) {
+		  if(response) return response;
+		  fetch(event.request).then(function(response) {
+			cache.put(event.request, response.clone());
+			return response;
+		  })
+		});
+	})
+ );
+});
